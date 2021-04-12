bdd6fac867109ce2ab401a72f5ef724742757ff9
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-12 10:03:02 +0300

    Implement code

--- src/text-for-role.js
+++ src/text-for-role.js
@@ -1,5 +1,27 @@
 const textForRole = (roles, textLines) => {
-    // Your implementation here
+    let result = '';
+    let textArr = textLines.split('\n');
+
+    for (let i = 0; i < roles.length; i++) {
+        const role = roles[i];
+        result = `${result}${role}:\n`;
+        let findedRole = false;
+
+        for (let k = 0; k < textArr.length; k++) {
+            const textLine = textArr[k];
+            const position = textLine.indexOf(role);
+            if (position === -1)
+                continue;
+
+            findedRole = true;
+            result = `${result}${k + 1}) ${textLine.slice(position + role.length + 2)}\n`;
+        }
+
+        if (findedRole)
+            result =`${result}\n`;    
+    }
+
+    return result;
 }
 
 module.exports = textForRole;
